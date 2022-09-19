015df94c4af32251ccf8a1be6e049da89a25aa54
kr_kredit <spirtt1985@yandex.ru>
2022-09-19 15:44:34 +0300

    Добавил дата класс Ticket

---
+++ src/main/java/ru/netology/Ticket.java
@@ -0,0 +1,38 @@
+package ru.netology;
+
+public class Ticket {
+    private int id;
+    private int price;
+    private String departureAirport;
+    private String arrivalAirport;
+    private int travelTime;
+
+
+    public Ticket(int id, int price, String departureAirport, String arrivalAirport, int travelTime) {
+        this.id = id;
+        this.price = price;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.travelTime = travelTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public int getTravelTime() {
+        return travelTime;
+    }
+}
